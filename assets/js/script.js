22dd553a1b228c6e42f18b9867361b511ea8621b
TorySnopl <Paramedic064@gmail.com>
2023-11-06 21:25:36 -0800

    changing over to oregon trail theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,27 @@
 const nextScreen = document.querySelector('.submit');
 const wrapper = document.querySelector('.openWrapper');
 
+
 function addAnimate(){  
-    wrapper.classList.add('animate__animated animate__fadeOutLeft animate__slow');
+   
+    wrapper.classList.add('animate__animated');
+    wrapper.classList.add('animate__fadeOutLeft');
+    wrapper.classList.add('animate__fast');
+   
+   
+    console.log('classes added');
 };
 
 
 
-nextScreen.addEventListener(onclick, function(event){
+
+nextScreen.addEventListener('click', function(event){
+
     event.preventDefault();
+
     addAnimate({once: true});
+
+    window.location.replace('./views/directory.html');
     
 
-
-
-})
+});
